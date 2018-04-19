7bada8e122d83c7d232d3090a57e3378d4f7fc61
Colleen O'Reilly <colleen@jelcreative.com>
2018-04-19 10:57:46 -0400

    testing scrollmagic

--- js/main.js
+++ js/main.js
@@ -1,5 +1,45 @@
 console.log('linked')
 
 $(document).ready(function () {
-  console.log('hi')
+
+  //Set up controller
+  var controller = new ScrollMagic.Controller()
+
+  //Left blocks scroll
+  $('.scroll-blocks .left-content').each(function(){
+    var itemScroll = this;
+    var scene = new ScrollMagic.Scene({
+      triggerElement: this,
+      triggerHook: 'onEnter',
+      offset: '150',
+      duration: '150%'
+    })
+    .setTween(TweenMax.to(this, 1, {y: '-40%', ease:Power0.easeNone}))
+    .addTo(controller)
+  })
+
+  //Right blocks scroll
+  $('.scroll-blocks .right-content').each(function(){
+    var itemScroll = this;
+    var scene = new ScrollMagic.Scene({
+      triggerElement: this,
+      triggerHook: 'onEnter',
+      offset: '150',
+      duration: '150%'
+    })
+    .setTween(TweenMax.to(this, 1, {y: '-20%', ease:Power0.easeNone}))
+    .addTo(controller)
+  })
+
+  // //Set up scene
+  // var blockScroll = new ScrollMagic.Scene({
+  //   //element used to start animation
+  //   triggerElement: ".scroll-blocks",
+  //     //where on screen it should start
+  //   triggerHook: 'onCenter',
+  //   duration: '100%'
+  //  })
+  //  .setTween(TweenMax.to('.scroll-blocks .left-content', 1, {y: '-80%', ease:Power0.easeNone}))
+  //  .addTo(controller)
+
 })
