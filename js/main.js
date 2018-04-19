28d94cd2bc793b0336e860eda79b7d208329f39c
Colleen O'Reilly <colleen@jelcreative.com>
2018-04-19 11:27:31 -0400

    added side slide in

--- js/main.js
+++ js/main.js
@@ -5,31 +5,40 @@ $(document).ready(function () {
   //Set up controller
   var controller = new ScrollMagic.Controller()
 
-  //Left blocks scroll
-  $('.scroll-blocks .left-content').each(function(){
-    var itemScroll = this;
+  //Scroll Block
+  $('.scroll-blocks').each(function(){
+    var itemScroll = $(this).find('.left-content');
     var scene = new ScrollMagic.Scene({
       triggerElement: this,
       triggerHook: 'onEnter',
       offset: '-150',
       duration: '180%'
     })
-    .setTween(TweenMax.to(this, 1, {y: '-100%', ease:Power0.easeNone}))
+    .setTween(TweenMax.to(itemScroll, 1, {y: '-100%', ease:Power0.easeNone}))
     .addTo(controller)
   })
 
-  //Side Blocks - Enter
-  // $('.scroll-blocks .left-content').each(function(){
-  //   var itemScroll = this;
-  //   var scene = new ScrollMagic.Scene({
-  //     triggerElement: this,
-  //     triggerHook: 'onEnter',
-  //     offset: '-150',
-  //     duration: '150%'
-  //   })
-  //   .setTween(TweenMax.to(this, 1, {y: '-50%', ease:Power0.easeNone}))
-  //   .addTo(controller)
-  // })
+  // Side Blocks - Enter
+  $('.side-blocks').each(function(){
+    var itemRight = $(this).find('.right-block')
+    var itemLeft = $(this).find('.left-block')
+
+    var tweenSlideIn = new TimelineMax()
+    var itemLeft = TweenMax.from(itemLeft, 1,{ x: '-30%' , opacity: 0});
+    var itemRight = TweenMax.from(itemRight, 1,{ x: '30%' , opacity: 0});
+      tweenSlideIn
+        .add(itemLeft)
+        .add(itemRight, 0.2);
+
+    var scene = new ScrollMagic.Scene({
+      triggerElement: this,
+      triggerHook: 'onEnter',
+      reverse: true,
+      duration: '100%'
+    })
+    .setTween(tweenSlideIn)
+    .addTo(controller)
+  })
 
   // //Right blocks scroll
   // $('.scroll-blocks .right-content').each(function(){
