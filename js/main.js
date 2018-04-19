1f80115e9ac7b941d880f9d84309e6b8dfb587e7
Colleen O'Reilly <colleen@jelcreative.com>
2018-04-19 11:48:37 -0400

    updated styles

--- js/main.js
+++ js/main.js
@@ -7,14 +7,14 @@ $(document).ready(function () {
 
   //Scroll Block
   $('.scroll-blocks').each(function(){
-    var itemScroll = $(this).find('.left-content');
+    var itemScroll = $(this).find('.highlight-block');
     var scene = new ScrollMagic.Scene({
       triggerElement: this,
       triggerHook: 'onEnter',
       offset: '-150',
       duration: '180%'
     })
-    .setTween(TweenMax.to(itemScroll, 1, {y: '-100%', ease:Power0.easeNone}))
+    .setTween(TweenMax.to(itemScroll, 1, {y: '-100%', ease:Linear.easeNone}))
     .addTo(controller)
   })
 
@@ -34,34 +34,11 @@ $(document).ready(function () {
       triggerElement: this,
       triggerHook: 'onEnter',
       reverse: true,
-      duration: '100%'
+      duration: '120%'
     })
     .setTween(tweenSlideIn)
     .addTo(controller)
   })
 
-  // //Right blocks scroll
-  // $('.scroll-blocks .right-content').each(function(){
-  //   var itemScroll = this;
-  //   var scene = new ScrollMagic.Scene({
-  //     triggerElement: this,
-  //     triggerHook: 'onEnter',
-  //     offset: '150',
-  //     duration: '150%'
-  //   })
-  //   .setTween(TweenMax.to(this, 1, {y: '-20%', ease:Power0.easeNone}))
-  //   .addTo(controller)
-  // })
-
-  // //Set up scene
-  // var blockScroll = new ScrollMagic.Scene({
-  //   //element used to start animation
-  //   triggerElement: ".scroll-blocks",
-  //     //where on screen it should start
-  //   triggerHook: 'onCenter',
-  //   duration: '100%'
-  //  })
-  //  .setTween(TweenMax.to('.scroll-blocks .left-content', 1, {y: '-80%', ease:Power0.easeNone}))
-  //  .addTo(controller)
 
 })
